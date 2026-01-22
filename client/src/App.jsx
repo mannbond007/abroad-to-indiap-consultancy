48bce67e4050484e0f68356cd3d753598b99ecab
sysrover007 <thelearning007@gmail.com>
2026-01-22 19:18:12 +0530

    app test file added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen w-screen bg-slate-950 text-white flex justify-center items-center'> 
+      <h1 className='text-4xl font-bold'>Welcome to Admission Consultancy</h1>
+    </div>
   )
 }
 
